04fc07f53c208f8313f6ad963d3d32ee1e2b8383
Morgan McLeod <morganmcleod1123@gmail.com>
2021-05-24 19:19:12 -0500

    Removed Hard-Coded variable from enemy script

--- SummerGame/Assets/Scripts/Enemy.cs
+++ SummerGame/Assets/Scripts/Enemy.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int health;
-    public float speed;
+    private float speed;
+    public float startSpeed;
+    private float dazedTime;
+    public float hitstun;
 
     void Start()
     {
-        
+        speed = startSpeed;
     }
 
     void Update()
     {
+        if(dazedTime <= 0)
+        {
+            speed = startSpeed;
+        } else
+        {
+            speed = 0;
+            dazedTime -= Time.deltaTime;
+        }
         if(health <= 0)
         {
             Destroy(gameObject);
@@ -23,6 +34,7 @@ public class Enemy : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+        dazedTime = hitstun;
         health -= damage;
         Debug.Log("Took " + damage + " damage");
     }
