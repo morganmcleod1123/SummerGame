ca42b3bddde750b54a79a7512565d48b120e9ca0
Morgan McLeod <morganmcleod1123@gmail.com>
2021-06-17 17:32:22 -0500

    Implemented Player Blink and framework for Dash
    - Can hit left shift to instantly blink to a set location
    - Work in progress: Hitting left control should make the player dash. Right now it just cancels the player's gravity.

--- SummerGame/Assets/Scripts/PlayerAbilities.cs
+++ SummerGame/Assets/Scripts/PlayerAbilities.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class PlayerAbilities : MonoBehaviour
 {
+    private GameManager gm;
+    private Rigidbody2D rigidbody2D;
     public GameObject projectile;
     public Vector2 velocity;
     public Vector2 offset = new Vector2(0.4f, 0.1f);
     bool faceRight;
 
     public float fireBallManaCost;
-    private GameManager gm;
+    public float teleportManaCost;
+
+    public GameObject teleportPos;
+    private int dashCount;
+    public float dashDistance = 15f;
+    bool isDashing;
 
     private void Start()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        isDashing = false;
         gm = GameManager.Instance;
     }
     void Update()
@@ -41,6 +50,44 @@ public class PlayerAbilities : MonoBehaviour
                 gm.decreaseMana(fireBallManaCost);
             }
         }
-        Debug.Log(faceRight);
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (GameManager.Instance.enoughMana(teleportManaCost))
+            {
+                blink();
+                gm.decreaseMana(teleportManaCost);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isDashing && faceRight)
+        {
+            Debug.Log("Right Dash");
+            StartCoroutine(Dash(1f));
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isDashing && !faceRight)
+        {
+            Debug.Log("Left Dash");
+            StartCoroutine(Dash(-1f));
+        }
+    }
+    IEnumerator Dash(float direction)
+    {
+        Debug.Log("Inside the Dash");
+        isDashing = true;
+        //dashCount--;
+        //trail.emitting = true;
+        //dust.Play();
+        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0f);
+        rigidbody2D.AddForce(new Vector2(dashDistance * direction, 0f), ForceMode2D.Impulse);
+        float gravity = rigidbody2D.gravityScale;
+        rigidbody2D.gravityScale = 0;
+        yield return new WaitForSeconds(0.4f);
+        //trail.emitting = false;
+        isDashing = false;
+        rigidbody2D.gravityScale = gravity;
+    }
+
+    void blink()
+    {
+        transform.position = teleportPos.transform.position;
     }
 }
