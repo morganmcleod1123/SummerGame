99cef070bf6166c7149c9294afa4fcb455330748
Jackson <jackson.pierce210@gmail.com>
2021-06-17 17:59:18 -0500

    AI Patrol Updates, Working Patrol, Chase, and Return
    AI now patrols between two walls with his "ground check position" circle, will chase player upon getting close, and will return to starting position and patrol again once he loses line of sight.

--- SummerGame/Assets/Scripts/AIpatrol.cs
+++ SummerGame/Assets/Scripts/AIpatrol.cs
@@ -6,12 +6,15 @@ public class AIpatrol : MonoBehaviour
 {
 
     public bool activePatrol;
+    public bool activeChase;
     public Rigidbody2D rigidb;
     public float walkSpeed;
     public bool activeTurn;
     public Transform groundCheckPosition;
-    public LayerMask groundLayer;
+    public LayerMask wallLayer;
     public bool enemyFacingRight = true;
+    public float startPos;
+    public bool backAtStart = false;
 
     [SerializeField]
     Transform player;
@@ -25,12 +28,14 @@ public class AIpatrol : MonoBehaviour
     void Start()
     {
         rigidb = GetComponent<Rigidbody2D>();
-        activePatrol = false;
+        activePatrol = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //print(transform.position.x);
+
         if (activePatrol)
         {
             Patrol();
@@ -42,12 +47,20 @@ public class AIpatrol : MonoBehaviour
         if (playerDistance < detectRange)
         {
             //detection and follow
+            activePatrol = false;
             Chase();
+            
         }
-        else
+        else if (activeChase == true && playerDistance > detectRange)
         {
             //stop following
             StopChase();
+        }
+
+        //If enemy is not patrolling and doesn't detect player
+        if (!activePatrol && !activeChase)
+        {
+            ReturnToStart();
         }
     }
 
@@ -55,8 +68,9 @@ public class AIpatrol : MonoBehaviour
     {
         if (activePatrol)
         {
-            activeTurn = !Physics2D.OverlapCircle(groundCheckPosition.position, 0.1f, groundLayer);
+            activeTurn = Physics2D.OverlapCircle(groundCheckPosition.position, 0.1f, wallLayer);
         }
+        print(enemyFacingRight);
     }
 
     void Patrol()
@@ -74,35 +88,101 @@ public class AIpatrol : MonoBehaviour
         transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
         walkSpeed *= -1;
         activePatrol = true;
+        enemyFacingRight = !enemyFacingRight;
     }
 
     void Chase()
     {
-
+        activeChase = true;
         if(transform.position.x < player.position.x)
         {
             //enemy is left of the player
-            rigidb.velocity = new Vector2(followSpeed, 0);
             if (!enemyFacingRight)
             {
                 transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
                 enemyFacingRight = true;
+                walkSpeed *= -1;
             }
+            rigidb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rigidb.velocity.y);
         }
         else if (transform.position.x > player.position.x)
         {
             //enemy is right of the player
-            rigidb.velocity = new Vector2(-followSpeed, 0);
             if (enemyFacingRight)
             {
                 transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
                 enemyFacingRight = false;
+                walkSpeed *= -1;
             }
+            rigidb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rigidb.velocity.y);
         }
     }
 
     void StopChase()
     {
+        activeChase = false;
         rigidb.velocity = new Vector2(0, 0);
+    }
+
+    void ReturnToStart()
+    {
+        //Enemy is left of starting position
+        if (transform.position.x < startPos - 0.2)
+        {
+            if (!enemyFacingRight)
+            {
+                transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+                enemyFacingRight = true;
+                walkSpeed *= -1;
+            }
+            rigidb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rigidb.velocity.y);
+        }
+        //Enemy is right of starting position
+        else if (transform.position.x > startPos + 0.2)
+        {
+            if (enemyFacingRight)
+            {
+                transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+                enemyFacingRight = false;
+                walkSpeed *= -1;
+            }
+            rigidb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rigidb.velocity.y);
+        }
+        else
+        {
+            if (!enemyFacingRight)
+            {
+                Flip();
+            }
+            rigidb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rigidb.velocity.y);
+            activePatrol = true;
+        }
+    }
+
+    void ReturnFromLeft()
+    {
+            if (!enemyFacingRight)
+            {
+                transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+                enemyFacingRight = true;
+            }
+            rigidb.velocity = new Vector2(followSpeed, 0);
+        if (transform.position.x > startPos)
+        {
+            activePatrol = true;
+        }
+    }
+
+    void ReturnFromRight()
+    {
+        while (transform.position.x > startPos)
+        {
+            if (enemyFacingRight)
+            {
+                transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+                enemyFacingRight = false;
+            }
+            rigidb.velocity = new Vector2(-followSpeed, 0);
+        }
     }
 }
